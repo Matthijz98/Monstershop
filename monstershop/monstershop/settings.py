a80081b629447dc4d540dd0b8abc41029a380bb4
Matthijs Zondervan <mzondervan@hotmail.nl>
2020-02-18 21:31:25 +0100

    set static root and project dir

--- monstershop/monstershop/settings.py
+++ monstershop/monstershop/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -134,8 +134,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+##########################################
+# Static files (CSS, JavaScript, Images) #
+##########################################
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
