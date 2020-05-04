7a05e15359b44057865e0207f81103480b1aba61
Matthijs Zondervan <mzondervan@hotmail.nl>
2020-05-04 18:56:28 +0200

    basic stock management

--- monstershop/stock_management/admin.py
+++ monstershop/stock_management/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import BuyInOrder, BuyInOrderDetail, BuyInOrderReceivement, Producer
 
 # Register your models here.
+
+
+class ProducerAdmin(admin.ModelAdmin):
+    search_fields = ["producer_name"]
+
+class BuyInOrderDetailAdmin(admin.TabularInline):
+    model = BuyInOrderDetail
+    autocomplete_fields = ["buy_in_order_detail_product"]
+
+
+class BuyInOrderAdmin(admin.ModelAdmin):
+    inlines = [BuyInOrderDetailAdmin]
+    autocomplete_fields = ["buy_in_order_producer"]
+
+admin.site.register(BuyInOrderReceivement)
+admin.site.register(BuyInOrderDetail)
+admin.site.register(BuyInOrder, BuyInOrderAdmin)
+admin.site.register(Producer, ProducerAdmin)
